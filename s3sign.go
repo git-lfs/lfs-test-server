e904c3ad2d3870f4e9007b65c5c3c5d714d0dc7d
rubyist <scott.barron@github.com>
2015-01-09 17:51:55 -0500

    Don't bother using content-md5

--- s3sign.go
+++ s3sign.go
@@ -3,20 +3,18 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha256"
-	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"net/http"
 	"time"
 )
 
 var (
-	// TODO Might need a way to remove content-md5 if the client doesn't give us one
-	signedHeaders = "content-md5;date;host;x-amz-content-sha256"
+	signedHeaders = "date;host;x-amz-content-sha256"
 )
 
 const (
-	dateLayout    = "20060102"
-	iso8601Layout = "20060102T000000Z"
+	dateLayout = "20060102"
 )
 
 type S3Token struct {
@@ -25,9 +23,9 @@ type S3Token struct {
 	Time     time.Time
 }
 
-func S3NewToken(method, path, sha, md5 string) *S3Token {
+func S3NewToken(method, path, sha string) *S3Token {
 	t := time.Now().UTC()
-	token := S3SignedToken(method, path, sha, md5, t)
+	token := S3SignedToken(method, path, sha, t)
 	location := fmt.Sprintf("https://%s.s3.amazonaws.com%s", Config.AwsBucket, path)
 	return &S3Token{
 		Token:    token,
@@ -36,14 +34,14 @@ func S3NewToken(method, path, sha, md5 string) *S3Token {
 	}
 }
 
-func S3SignedToken(method, path, sha, md5 string, t time.Time) string {
-	canonicalRequest := CanonicalRequest(method, path, sha, md5, t)
+func S3SignedToken(method, path, sha string, t time.Time) string {
+	canonicalRequest := CanonicalRequest(method, path, sha, t)
 	stringToSign := StringToSign(canonicalRequest, t)
 	signature := Signature(stringToSign, t)
 
 	return fmt.Sprintf("AWS4-HMAC-SHA256 Credential=%s/%s/%s/s3/aws4_request,SignedHeaders=%s,Signature=%s",
 		Config.AwsKey,
-		date(t),
+		t.Format(dateLayout),
 		Config.AwsRegion,
 		signedHeaders,
 		signature,
@@ -51,7 +49,7 @@ func S3SignedToken(method, path, sha, md5 string, t time.Time) string {
 }
 
 func Signature(sts string, t time.Time) string {
-	kDate := hmacSha256([]byte(fmt.Sprintf("AWS4%s", Config.AwsSecretKey)), []byte(date(t)))
+	kDate := hmacSha256([]byte(fmt.Sprintf("AWS4%s", Config.AwsSecretKey)), []byte(t.Format(dateLayout)))
 	kRegion := hmacSha256(kDate, []byte(Config.AwsRegion))
 	kService := hmacSha256(kRegion, []byte("s3"))
 	kCreds := hmacSha256(kService, []byte("aws4_request"))
@@ -60,19 +58,18 @@ func Signature(sts string, t time.Time) string {
 
 func StringToSign(request string, t time.Time) string {
 	return fmt.Sprintf("AWS4-HMAC-SHA256\n%s\n%s/%s/s3/aws4_request\n%s",
-		httpdate(t),
-		date(t),
+		t.Format(http.TimeFormat),
+		t.Format(dateLayout),
 		Config.AwsRegion,
 		sha256Hex([]byte(request)),
 	)
 }
 
-func CanonicalRequest(method, path, sha, md5 string, t time.Time) string {
-	return fmt.Sprintf("%s\n%s\n\ncontent-md5:%s\ndate:%s\nhost:%s.s3.amazonaws.com\nx-amz-content-sha256:%s\n\n%s\n%s",
+func CanonicalRequest(method, path, sha string, t time.Time) string {
+	return fmt.Sprintf("%s\n%s\n\ndate:%s\nhost:%s.s3.amazonaws.com\nx-amz-content-sha256:%s\n\n%s\n%s",
 		method,
 		path,
-		encodedMd5(md5),
-		httpdate(t),
+		t.Format(http.TimeFormat),
 		Config.AwsBucket,
 		sha,
 		signedHeaders,
@@ -90,26 +87,8 @@ func hmacSha256Hex(key, message []byte) string {
 	return hex.EncodeToString(hmacSha256(key, message))
 }
 
-func date(t time.Time) string {
-	return t.Format(dateLayout)
-}
-
-func httpdate(t time.Time) string {
-	return t.Format(time.RFC1123)
-}
-
-func iso8601(t time.Time) string {
-	return t.Format(iso8601Layout)
-}
-
 func sha256Hex(value []byte) string {
 	hash := sha256.New()
 	hash.Write(value)
 	return hex.EncodeToString(hash.Sum(nil))
-}
-
-// Takes a hex encoded representation of an md5sum, decodes and base64 encodes it
-func encodedMd5(value string) string {
-	v, _ := hex.DecodeString(value) // TODO: handle the error
-	return base64.StdEncoding.EncodeToString(v)
 }
--- s3sign_test.go
+++ s3sign_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"testing"
 	"time"
 )
@@ -10,7 +8,6 @@ import (
 var (
 	content    = "Welcome to Amazon S3."
 	contentSha string
-	contentMd5 string
 	path       = "/test%24file.text"
 	now        time.Time
 )
@@ -18,7 +15,7 @@ var (
 func TestCanonicalRequest(t *testing.T) {
 	testSetup()
 
-	cr := CanonicalRequest("PUT", path, contentSha, contentMd5, now)
+	cr := CanonicalRequest("PUT", path, contentSha, now)
 	if cr != expectedCanonicalRequest {
 		t.Fatalf("incorrect canonical request\nexpected:\n%s\n\ngot:\n%s", expectedCanonicalRequest, cr)
 	}
@@ -27,7 +24,7 @@ func TestCanonicalRequest(t *testing.T) {
 func TestStringToSign(t *testing.T) {
 	testSetup()
 
-	cr := CanonicalRequest("PUT", path, contentSha, contentMd5, now)
+	cr := CanonicalRequest("PUT", path, contentSha, now)
 	sts := StringToSign(cr, now)
 	if sts != expectedStringToSign {
 		t.Fatalf("incorrect string to sign\nexpected:\n%s\n\ngot:\n%s", expectedStringToSign, sts)
@@ -37,7 +34,7 @@ func TestStringToSign(t *testing.T) {
 func TestSignature(t *testing.T) {
 	testSetup()
 
-	cr := CanonicalRequest("PUT", path, contentSha, contentMd5, now)
+	cr := CanonicalRequest("PUT", path, contentSha, now)
 	sts := StringToSign(cr, now)
 	signature := Signature(sts, now)
 	if signature != expectedSignature {
@@ -48,7 +45,7 @@ func TestSignature(t *testing.T) {
 func TestS3SignedToken(t *testing.T) {
 	testSetup()
 
-	token := S3SignedToken("PUT", path, contentSha, contentMd5, now)
+	token := S3SignedToken("PUT", path, contentSha, now)
 	if token != expectedToken {
 		t.Fatalf("incorrect token\nexpected:\n%s\n\ngot:\n%s", expectedToken, token)
 	}
@@ -57,9 +54,9 @@ func TestS3SignedToken(t *testing.T) {
 func TestS3Token(t *testing.T) {
 	testSetup()
 
-	token := S3NewToken("PUT", path, contentSha, contentMd5)
+	token := S3NewToken("PUT", path, contentSha)
 
-	if token.Token != S3SignedToken("PUT", path, contentSha, contentMd5, token.Time) {
+	if token.Token != S3SignedToken("PUT", path, contentSha, token.Time) {
 		t.Fatalf("incorrect token signature")
 	}
 
@@ -74,11 +71,6 @@ func testSetup() {
 	Config.AwsBucket = "examplebucket"
 
 	contentSha = sha256Hex([]byte(content))
-
-	hash := md5.New()
-	hash.Write([]byte(content))
-	contentMd5 = hex.EncodeToString(hash.Sum(nil))
-
 	now, _ = time.Parse(time.RFC822, "24 May 13 00:00 GMT")
 }
 
@@ -86,22 +78,21 @@ const (
 	expectedCanonicalRequest = `PUT
 /test%24file.text
 
-content-md5:1EfQ6PKJ8WoS/2AnznfCWA==
 date:Fri, 24 May 2013 00:00:00 GMT
 host:examplebucket.s3.amazonaws.com
 x-amz-content-sha256:44ce7dd67c959e0d3524ffac1771dfbba87d2b6b4b4e99e42034a8b803f8b072
 
-content-md5;date;host;x-amz-content-sha256
+date;host;x-amz-content-sha256
 44ce7dd67c959e0d3524ffac1771dfbba87d2b6b4b4e99e42034a8b803f8b072`
 
 	expectedStringToSign = `AWS4-HMAC-SHA256
 Fri, 24 May 2013 00:00:00 GMT
 20130524/us-east-1/s3/aws4_request
-35918b6d5f2f402b12a82af55c3e432bd99cdc840c8553ced35891fbedd9c2fc`
+627af852f282e30ce4aa940ee749726c031dfeb5fb8d793c0bf86128165d9e2b`
 
-	expectedSignature = `b765307d50cb6df6018156c64491b2f177dcb4484655926e9a7d7105bdc5fb87`
+	expectedSignature = `6eb5d2a63d219da38ce1e6d23114b5800721dc63c2b66dca4f7612d0fb13f975`
 
-	expectedToken = `AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=content-md5;date;host;x-amz-content-sha256,Signature=b765307d50cb6df6018156c64491b2f177dcb4484655926e9a7d7105bdc5fb87`
+	expectedToken = `AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=date;host;x-amz-content-sha256,Signature=6eb5d2a63d219da38ce1e6d23114b5800721dc63c2b66dca4f7612d0fb13f975`
 
 	expectedLocation = `https://examplebucket.s3.amazonaws.com/test%24file.text`
 )
