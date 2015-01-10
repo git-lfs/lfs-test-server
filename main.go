9060940a070b23ab70d76da71f7518f33fa62eba
rubyist <scott.barron@github.com>
2015-01-09 19:08:57 -0500

    start sketching out handlers

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
@@ -11,14 +13,40 @@ func main() {
 
 	s := router.Path("/{user}/{repo}/objects/{oid}").Subrouter()
 
-	s.Methods("PUT").HandlerFunc(PutHandler)
-	s.Methods("GET").HandlerFunc(GetHandler)
+	s.Methods("GET").Headers("Accept", "application/vnd.git-media").HandlerFunc(GetHandler)
+	s.Methods("GET").Headers("Accept", "application/vnd.git-media+json").HandlerFunc(GetUploadHandler)
 
 	log.Fatal(http.ListenAndServe(":8083", router))
 }
 
-func PutHandler(w http.ResponseWriter, r *http.Request) {
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
-func GetHandler(w http.ResponseWriter, r *http.Request) {
+func GetUploadHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	oid := vars["oid"]
+	path := oidPath(oid)
+	token := S3NewToken("PUT", path, oid)
+
+	m := make(map[string]map[string]string)
+	header := make(map[string]string)
+	header["Date"] = token.Time.Format(http.TimeFormat)
+	header["Authorization"] = token.Token
+	header["x-amz-content-sha256"] = oid
+	m["header"] = header
+
+	links := make(map[string]string)
+	links["upload"] = token.Location
+	links["callback"] = "http://somecallback.com"
+	m["_links"] = links
+
+	enc := json.NewEncoder(w)
+	enc.Encode(m)
+}
+
+func oidPath(oid string) string {
+	dir := filepath.Join(oid[0:2], oid[2:4])
+
+	return filepath.Join("/", dir, oid)
 }
