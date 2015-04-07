1095ec06937668ed5755f13c89824db88cc1c1bb
rubyist <scott.barron@github.com>
2015-04-07 14:16:54 -0400

    Make the management interface look a little nicer

--- mgmt.go
+++ mgmt.go
@@ -4,15 +4,48 @@ import (
 	"errors"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 
+	"github.com/GeertJohan/go.rice"
 	"github.com/gorilla/mux"
 )
 
+var (
+	cssBox      *rice.Box
+	templateBox *rice.Box
+)
+
+type pageData struct {
+	Name   string
+	Config *Configuration
+	Users  []*MetaUser
+}
+
 func (a *App) addMgmt(r *mux.Router) {
 	r.HandleFunc("/mgmt", basicAuth(a.indexHandler)).Methods("GET")
+	r.HandleFunc("/mgmt/users", basicAuth(a.usersHandler)).Methods("GET")
 	r.HandleFunc("/mgmt/add", basicAuth(a.addUserHandler)).Methods("POST")
 	r.HandleFunc("/mgmt/del", basicAuth(a.delUserHandler)).Methods("POST")
+
+	cssBox = rice.MustFindBox("mgmt/css")
+	templateBox = rice.MustFindBox("mgmt/templates")
+	r.HandleFunc("/mgmt/css/{file}", cssHandler)
+}
+
+func cssHandler(w http.ResponseWriter, r *http.Request) {
+	file := mux.Vars(r)["file"]
+	f, err := cssBox.Open(file)
+	if err != nil {
+		http.Error(w, "Not Found", 404)
+		logRequest(r, 404)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/css")
+
+	io.Copy(w, f)
+	f.Close()
 }
 
 func basicAuth(h http.HandlerFunc) http.HandlerFunc {
@@ -44,12 +77,43 @@ func basicAuth(h http.HandlerFunc) http.HandlerFunc {
 }
 
 func (a *App) indexHandler(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.New("main").Parse(bodyTemplate))
-	t.New("body").Parse(indexTemplate)
-
-	type lfs struct {
-		Users []*MetaUser
+	bodyString, err := templateBox.String("body.tmpl")
+	if err != nil {
+		http.Error(w, "Not Found", 404)
+		logRequest(r, 404)
+		return
 	}
+
+	configString, err := templateBox.String("config.tmpl")
+	if err != nil {
+		http.Error(w, "Not Found", 404)
+		logRequest(r, 404)
+		return
+	}
+
+	t := template.Must(template.New("main").Parse(bodyString))
+	t.New("content").Parse(configString)
+
+	t.Execute(w, pageData{Name: "index", Config: Config})
+}
+
+func (a *App) usersHandler(w http.ResponseWriter, r *http.Request) {
+	bodyString, err := templateBox.String("body.tmpl")
+	if err != nil {
+		http.Error(w, "Not Found", 404)
+		logRequest(r, 404)
+		return
+	}
+
+	contentString, err := templateBox.String("users.tmpl")
+	if err != nil {
+		http.Error(w, "Not Found", 404)
+		logRequest(r, 404)
+		return
+	}
+
+	t := template.Must(template.New("main").Parse(bodyString))
+	t.New("content").Parse(contentString)
 
 	users, err := a.metaStore.Users()
 	if err != nil {
@@ -57,9 +121,7 @@ func (a *App) indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	l := &lfs{Users: users}
-
-	t.Execute(w, l)
+	t.Execute(w, pageData{Name: "users", Users: users})
 }
 
 func (a *App) addUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -75,7 +137,7 @@ func (a *App) addUserHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, "/mgmt", 302)
+	http.Redirect(w, r, "/mgmt/users", 302)
 }
 
 func (a *App) delUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -90,7 +152,7 @@ func (a *App) delUserHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, "/mgmt", 302)
+	http.Redirect(w, r, "/mgmt/users", 302)
 }
 
 func authenticate(r *http.Request) error {
@@ -110,16 +172,6 @@ func authenticate(r *http.Request) error {
 	}
 	return err
 }
-
-var bodyTemplate = `<html>
-<head>
-	<title>Git LFS Server Management</title>
-</head>
-<body>
-{{template "body" .}}
-</body>
-</html>
-`
 
 var indexTemplate = `
 <h2>Users</h2>
