09b9bbbbb436cff6fdafc2dbc39cd6757183dbe9
rubyist <scott.barron@github.com>
2015-04-29 13:36:37 -0400

    bump version

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 const (
 	contentMediaType = "application/vnd.git-lfs"
 	metaMediaType    = contentMediaType + "+json"
-	version          = "0.1.2"
+	version          = "0.2.0"
 )
 
 var (
