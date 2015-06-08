e019e1009baf908f38c3d5d6bd81d5dca2b69a4a
Jericho gil Cruz Alfonso <Jericho25@users.noreply.github.com>
2015-06-08 09:15:11 -0700

    Update bolt_amd64.go

--- Godeps/_workspace/src/github.com/boltdb/bolt/bolt_amd64.go
+++ Godeps/_workspace/src/github.com/boltdb/bolt/bolt_amd64.go
@@ -5,10 +5,6 @@ const maxMapSize = 0xFFFFFFFFFFFF // 256TB
 
 // maxAllocSize is the size used when creating array pointers.
 const maxAllocSize = 0x7FFFFFFF
-{
-  "commit": "sha1",
-  "branch": "master",
-  "objects": [
-    { "oid": "abcdef" }, ...
-  ]
-}end..#jgaj
+{ admin:org, admin:org_hook, admin:public_key, admin:repo_hook, delete_repo, gist, notifications, repo, user
+  
+ #jgaj — admin:org, admin:org_hook, admin:public_key, admin:repo_hook, delete_repo, gist, notifications, repo, user
